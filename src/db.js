04ffcec8cd47c3a0b360f73570fa60af2b94c2e6
yaoleksa <mryaremchyk@gmail.com>
2024-01-11 13:49:54 +0200

    16 deploy

--- src/db.js
+++ src/db.js
@@ -2,24 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabase = createClient('https://azxmmelolclqfcfjgpka.supabase.co',
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6eG1tZWxvbGNscWZjZmpncGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwMTk0MTEsImV4cCI6MjAxNDU5NTQxMX0.nYQqwnBeFbD5qlQ6fpVxN8PH2Nvegtgga05wqEm3-y8');
 
-const signIn = () => {
-    return supabase.auth.signInWithPassword({
-        email: 'ekt_1@ukr.net',
-        password: 'notveryeasy4473'
-    });
-}
-
-const insertArticle = (article) => {
-    const requestResponse = supabase.from('main_article').select('id');
-    console.log(requestResponse);
-    // supabase.from('main_article').insert({
-    //     id: article.atrile_id,
-    //     title: article.title,
-    //     link: article.link,
-    //     content: article.content
-    // });
-}
-
 export default class DB {
     token;
     refreshToken;
@@ -35,10 +17,10 @@ export default class DB {
         })
     }
     insertArticle(Article) {
-        supabase.from('main_article').select('id').then(DBresponse => {
+        supabase.from('main_article').select('article_id').then(DBresponse => {
             if(DBresponse.data.length === 0 && Article) {
                 supabase.from('main_article').insert({
-                    id: Article.article_id,
+                    article_id: Article.article_id,
                     title: Article.title,
                     link: Article.link,
                     content: Article.content
@@ -54,7 +36,7 @@ export default class DB {
                     link: Article.link,
                     content: Article.content
                 }).match({
-                    id: DBresponse.data[0].id
+                    article_id: DBresponse.data[0].article_id
                 }).then(result => {
                     console.log(result);
                 })
@@ -63,4 +45,5 @@ export default class DB {
             console.log(error.message);
         })
     }
+    getSavedArticle
 }
