1611ae4c5dfe45b70db5a2a04ead60feb0bb4b29
yaoleksa <mryaremchyk@gmail.com>
2024-05-29 21:45:49 +0300

    First unit test has been successfuly created

---
+++ mocks/apis.js
@@ -0,0 +1,64 @@
+export default class Apis {
+    static getIPaddress() {
+        return new Promise((resolve, reject) => {
+            resolve("45.45.45.45");
+        });
+    }
+    static getGeoLocation(ip) {
+        return axios.get(`https://ipinfo.io/${ip}/geo`);
+    }
+    static getMainNew() {
+        return axios.get('https://free-news.p.rapidapi.com/v1/search?q=*&lang=uk', {
+            headers: {
+                'X-RapidAPI-Key': 'dc40d2b288msh88ced99c0191b37p144f83jsne853bb67a11f'
+            }
+        });
+    }
+    static getNews() {
+        return axios.get('https://newsdata.io/api/1/news', {
+            params: {
+                country: 'ua',
+                apikey: 'pub_8576a5096c78cacae47c5c74fd5c34419a6d'
+            }
+        }).catch(err => {
+            console.log(`and this one: ${err.message}`);
+            return DB.getSavedArticles(null);
+        })
+    }
+    static getWeather(location) {
+        const latLong = location.loc.split(',');
+        return axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latLong[0]}&longitude=${latLong[1]}&hourly=temperature_2m`);
+    }
+    static getExchangeRateCache() {
+        return axios.request({
+            method: 'GET',
+            url: 'https://exchange-rate-api1.p.rapidapi.com/latest',
+            params: {
+                base: 'UAH'
+            },
+            headers: {
+                'X-RapidAPI-Key': 'dc40d2b288msh88ced99c0191b37p144f83jsne853bb67a11f',
+                'X-RapidAPI-Host': 'exchange-rate-api1.p.rapidapi.com'
+              }
+        }).catch(error => {
+            console.log(error.message);
+            return DB.getSavedRates();
+        });
+    }
+    static getNewsByTopic(topic) {
+        return axios.request({
+            method: 'GET',
+            url: 'https://gnews.io/api/v4/search',
+            params: {
+                q: topic,
+                lang: 'ua',
+                country:'ua',
+                max: 10,
+                apikey: '985901b98d070eed7d957eda27580896'
+            }
+        }).catch(error => {
+            console.log(error.message);
+            return DB.getSavedArticles(topic);
+        })
+    }
+}
--- tests/all.test.js
+++ tests/all.test.js
@@ -1,5 +1,11 @@
 import Apis from '../src/apis';
+jest.mock('../mocks/apis');
 
-test('msg', () => {
-    expect(1+1).toBe(2);
-})
+it('test apis', () => {
+    expect.assertions(1);
+    return Apis.getIPaddress().then(response => {
+        expect(response.data).toMatchObject({
+            "ip": /[\d]+\.[\d]+\.[\d]+\.[\d]+/
+        });
+    });
+});
