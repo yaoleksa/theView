cc000ebef5ec22b5da55d19597c5a5e52313725a
yaoleksa <mryaremchyk@gmail.com>
2024-05-29 17:04:06 +0300

    almost config jest tests

--- jest.config.js
+++ jest.config.js
@@ -151,7 +151,7 @@ const config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -180,13 +180,15 @@ const config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "\\.[jt]sx?$": "babel-jest"
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "\\.pnp\\.[^\\/]+$"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
--- tests/all.test.js
+++ tests/all.test.js
@@ -1,3 +1,5 @@
+import index from '../src/index';
+
 test('msg', () => {
     expect(1+1).toBe(2);
 })
